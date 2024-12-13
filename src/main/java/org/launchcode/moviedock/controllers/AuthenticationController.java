e624df78048e71713e3dc58adec64d6888983def
Alex <alex@Alexs-MacBook-Air.local>
2024-12-13 00:52:59 -0600

    Login and Registration pages almost done, started on AuthenticationController, will finish another day

--- src/main/java/org/launchcode/moviedock/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/moviedock/controllers/AuthenticationController.java
@@ -3,8 +3,11 @@ package org.launchcode.moviedock.controllers;
 import jakarta.servlet.http.HttpSession;
 import org.launchcode.moviedock.models.User;
 import org.launchcode.moviedock.models.data.UserRepository;
+import org.launchcode.moviedock.models.dto.LoginFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Optional;
 
@@ -35,4 +38,12 @@ public class AuthenticationController {
     private static void setUserInSession(HttpSession session, User user) {
         session.setAttribute(userSessionKey, user.getId());
     }
+
+    @GetMapping("login")
+    public String login(Model model) {
+        model.addAttribute("title", "Login");
+        model.addAttribute(new LoginFormDTO());
+        return "login";
+    }
+
 }
