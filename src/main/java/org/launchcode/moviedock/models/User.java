85596e751d45908b3d9a05e4c1c735cd88f84c7c
Alex <alex@Alexs-MacBook-Air.local>
2024-12-12 23:21:21 -0600

    Update, filling out User mode. Created Abstract Entity

--- src/main/java/org/launchcode/moviedock/models/User.java
+++ src/main/java/org/launchcode/moviedock/models/User.java
@@ -1,5 +1,7 @@
 package org.launchcode.moviedock.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -7,14 +9,27 @@ import jakarta.persistence.Entity;
 
 
 @Entity
-public class User {
-
-    int id;
+public class User extends AbstractEntity {
 
     @NotNull(message = "This field is required")
-    @Size(min=3, max=45, message = "Username must be between 3 and 45 characters")
-    String username;
+    private String username;
 
+    @NotNull
+    private String pwHash;
+
+    @NotNull
     @Email
-    String email;
+    private String email;
+
+    public User () {}
+
+    public User (String username, String password) {
+        this.username = username;
+        this.pwHash = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
 }
