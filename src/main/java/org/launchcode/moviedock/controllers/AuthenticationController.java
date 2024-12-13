09df0c2e4199a79e99503d5d3f5972bb11dd4b23
Alex <alex@Alexs-MacBook-Air.local>
2024-12-12 23:39:11 -0600

    AuthenticationFilter

--- src/main/java/org/launchcode/moviedock/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/moviedock/controllers/AuthenticationController.java
@@ -1,4 +1,38 @@
 package org.launchcode.moviedock.controllers;
 
+import jakarta.servlet.http.HttpSession;
+import org.launchcode.moviedock.models.User;
+import org.launchcode.moviedock.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
 public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User gretUserFromSession(HttpSession session) {
+
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
 }
