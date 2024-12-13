52d7b5e84b38afee0cb211a907558a5acacbdc4e
Alex <alex@Alexs-MacBook-Air.local>
2024-12-12 22:54:32 -0600

    Started on User model, added MySQL gradle dependencies, updated application.properties to connect to MySQL database

--- src/main/java/org/launchcode/moviedock/models/User.java
+++ src/main/java/org/launchcode/moviedock/models/User.java
@@ -1,4 +1,20 @@
 package org.launchcode.moviedock.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.persistence.Entity;
+
+
+@Entity
 public class User {
+
+    int id;
+
+    @NotNull(message = "This field is required")
+    @Size(min=3, max=45, message = "Username must be between 3 and 45 characters")
+    String username;
+
+    @Email
+    String email;
 }
