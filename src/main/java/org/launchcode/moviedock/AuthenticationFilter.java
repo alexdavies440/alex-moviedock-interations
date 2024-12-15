175f74a1da8b53c1bfa745d577ed0f35d8564d77
Alex <alex@Alexs-MacBook-Air.local>
2024-12-14 21:50:32 -0600

    Started work on header, added ability to logout, added css file

--- src/main/java/org/launchcode/moviedock/AuthenticationFilter.java
+++ src/main/java/org/launchcode/moviedock/AuthenticationFilter.java
@@ -22,7 +22,7 @@ public class AuthenticationFilter implements HandlerInterceptor {
     AuthenticationController authenticationController;
 
     private static final List<String> whitelist =
-            Arrays.asList("/login", "/register", "/logout");
+            Arrays.asList("/login", "/register", "/logout", "/css/styles.css");
 
     private static boolean isWhitelisted(String path) {
         for (String pathRoot : whitelist) {
