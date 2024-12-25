4ed994a21751e41a99abeabb05315f82736d8c33
Alex <alex@Alexs-MacBook-Air.local>
2024-12-24 19:09:01 -0600

    Added UserDetailsService and AuthenticationProvider to WebSecurityConfig

--- src/main/java/org/launchcode/moviedock/config/WebSecurityConfig.java
+++ src/main/java/org/launchcode/moviedock/config/WebSecurityConfig.java
@@ -1,6 +1,10 @@
 package org.launchcode.moviedock.config;
 
+import com.mysql.cj.protocol.AuthenticationProvider;
+import org.launchcode.moviedock.service.AppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.userdetails.*;
@@ -10,7 +14,12 @@ import org.springframework.security.provisioning.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private AppUserDetailsService appUserDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(registry -> {
@@ -26,21 +35,34 @@ public class WebSecurityConfig {
 
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails appUser = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("user"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("USER", "ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(appUser, admin);
+//    }
+
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails appUser = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("user"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("USER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(appUser, admin);
+        return appUserDetailsService;
     }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(appUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
