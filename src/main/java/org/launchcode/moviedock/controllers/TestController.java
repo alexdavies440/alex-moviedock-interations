1da7588bf503f7f1eb8b627bee9b1cd60132dae1
Alex <alex@Alexs-MacBook-Air.local>
2024-12-25 18:31:38 -0600

    Re-introduced dynamic SIGN IN/SIGN OUT button but dramatically simplified with use of sec:authorize thymeleaf attribute instead of the complicated web of methods and model attributes before

---
+++ src/main/java/org/launchcode/moviedock/controllers/TestController.java
@@ -0,0 +1,26 @@
+//package org.launchcode.moviedock.controllers;
+//
+//import org.launchcode.moviedock.data.AppUserRepository;
+//import org.launchcode.moviedock.models.AppUser;
+//import org.launchcode.moviedock.models.dto.SignupFormDTO;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class TestController {
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    private AppUserRepository appUserRepository;
+//
+//
+//
+//}
