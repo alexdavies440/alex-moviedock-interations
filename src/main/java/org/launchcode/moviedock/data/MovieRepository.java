2f35849aefee86c20cf6c1a7aed85fcf54c0d85d
nandinigit33 <nandinids33@gmail.com>
2025-01-03 23:04:07 -0500

    deleted unused imports from the MovieRepository and UserRespository classes

--- src/main/java/org/launchcode/moviedock/data/MovieRepository.java
+++ src/main/java/org/launchcode/moviedock/data/MovieRepository.java
@@ -1,7 +1,6 @@
 package org.launchcode.moviedock.data;
 
 import org.launchcode.moviedock.models.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
