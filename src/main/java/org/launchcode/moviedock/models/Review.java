f9543f17764207c45ae21d2a4ac0a74dc90d41ea
arunastanley <arunapaulus@gmail.com>
2025-01-04 04:53:14 -0600

    Removing unwanted import

--- src/main/java/org/launchcode/moviedock/models/Review.java
+++ src/main/java/org/launchcode/moviedock/models/Review.java
@@ -3,7 +3,6 @@ package org.launchcode.moviedock.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
