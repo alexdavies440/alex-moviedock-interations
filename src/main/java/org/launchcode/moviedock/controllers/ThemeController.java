fdf6bd342705915789810e532eec8a2a353091fa
Alex <alex@Alexs-MacBook-Air.local>
2025-01-07 22:52:37 -0600

    fixed bug with wrong class field being called for navMode

--- src/main/java/org/launchcode/moviedock/controllers/ThemeController.java
+++ src/main/java/org/launchcode/moviedock/controllers/ThemeController.java
@@ -1,5 +1,6 @@
 package org.launchcode.moviedock.controllers;
 
+import org.launchcode.moviedock.models.Theme;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,21 +11,27 @@ public class ThemeController {
     @GetMapping("/test")
     public String home(Model model) {
 
-        String mode = "light";
-        String cssPath = "";
-        String navMode = "";
+//        String mode = "dark";
+//        String cssPath = "";
+//        String navMode = "";
+//
+//        if (mode.equals("dark")) {
+//            cssPath = "/css/styles.css";
+//            navMode = "navbar navbar-inverse navbar-fixed-top bar";
+//        }
+//        if (mode.equals("light")) {
+//            cssPath = "/css/styles-light.css";
+//            navMode = "navbar navbar-default navbar-fixed-top bar";
+//        }
+//
+//        model.addAttribute("cssPath", cssPath);
+//        model.addAttribute("navMode", navMode);
 
-        if (mode.equals("dark")) {
-            cssPath = "/css/styles.css";
-            navMode = "navbar navbar-inverse navbar-fixed-top bar";
-        }
-        if (mode.equals("light")) {
-            cssPath = "/css/styles-light.css";
-            navMode = "navbar navbar-default navbar-fixed-top bar";
-        }
 
-        model.addAttribute("cssPath", cssPath);
-        model.addAttribute("navMode", navMode);
+            Theme theme = new Theme("light");
+
+            model.addAttribute("cssPath", theme.getCssPath());
+            model.addAttribute("navMode", theme.getNavMode());
 
         return "theme-test";
     }
--- src/main/java/org/launchcode/moviedock/models/Theme.java
+++ src/main/java/org/launchcode/moviedock/models/Theme.java
@@ -6,14 +6,14 @@ public class Theme extends AbstractEntity {
     private String cssPath;
     private String navMode;
 
-    public Theme(String mode) {
-        this.mode = mode;
+    public Theme(String aMode) {
+        this.mode = aMode;
 
-        if (mode.equals("dark")) {
+        if (aMode.equals("dark")) {
             this.cssPath = "/css/styles.css";
             this.navMode = "navbar navbar-inverse navbar-fixed-top bar";
         }
-        if (mode.equals("light")) {
+        if (aMode.equals("light")) {
             this.cssPath = "/css/styles-light.css";
             this.navMode = "navbar navbar-default navbar-fixed-top bar";
         }
