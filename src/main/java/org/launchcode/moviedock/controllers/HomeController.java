f746853941335d8a73901d639de0360698290de2
Aruna <arunapaulus@gmail.com>
2025-01-23 12:11:01 -0600

    resolving merge conflicts

--- src/main/java/org/launchcode/moviedock/controllers/HomeController.java
+++ src/main/java/org/launchcode/moviedock/controllers/HomeController.java
@@ -5,7 +5,7 @@ import jakarta.validation.Valid;
 import org.launchcode.moviedock.data.AppUserRepository;
 import org.launchcode.moviedock.data.MovieRepository;
 
-import org.launchcode.moviedock.data.ReviewRepository;
+
 import org.launchcode.moviedock.models.AppUser;
 import org.launchcode.moviedock.models.Movie;
 import org.launchcode.moviedock.security.service.PrincipalService;
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.launchcode.moviedock.data.ReviewRepository;
 
 
 import java.util.ArrayList;
